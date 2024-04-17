34050abe5c6d7ace4980454be040ede8b63eeee2
yhammix <mansilla.y86@gmail.com>
2024-04-16 23:39:48 -1000

    Se agregan vistas parciales de Index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,13 +15,44 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            var persona = new Persona()
-            {
-                Nombre = "Yhamil Mansilla",
-                Edad = 19
-            };
+            var proyectos = ObtenerProyectos ().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            return View(modelo);
+        }
 
-            return View(persona);
+        private List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto
+                {
+                    Titulo="Amazon",
+                    Descripcion ="E-Commerce realizado en ASP.NET core",
+                    Link ="https://amazon.com",
+                    ImagenURL ="/imagenes/amazon.png"
+                },
+                new Proyecto
+                {
+                    Titulo="New York Times",
+                    Descripcion ="Pagina de noticias en React",
+                    Link ="https://nytimes.com",
+                    ImagenURL ="/imagenes/nyt.png"
+                },
+                new Proyecto
+                {
+                    Titulo="Reddit",
+                    Descripcion ="Red social para compartir en comunidades",
+                    Link ="https://reddit.com",
+                    ImagenURL ="/imagenes/reddit.png"
+                },
+                new Proyecto
+                {
+                    Titulo="Steam",
+                    Descripcion ="Tienda en linea para comprar video juegos",
+                    Link ="https://store.steampowered.com",
+                    ImagenURL ="/imagenes/nyt.png"
+                }
+            };
         }
 
         public IActionResult Privacy()
--- obj/Debug/net8.0/Portafolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portafolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portafolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c5889d0618ce86ddb8509d6fd590b044e052a9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portafolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portafolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
